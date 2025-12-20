4dedb672d42e331f28cea4b506ec5a9ff95761f3
Marco Tarantino <marco.tarantino.bg@gmail.com>
2025-12-21 00:48:19 +0100

    register broken

--- stormbrainer/frontend/src/pages/RegisterPage.jsx
+++ stormbrainer/frontend/src/pages/RegisterPage.jsx
@@ -38,7 +38,7 @@ const RegisterPage = () => {
     setIsLoading(true);
 
     try {
-      await register(email, password);
+      await register(username, email, password);
       navigate('/');
     } catch (err) {
       setError(err instanceof Error ? err.message : 'Registration failed');
